cbe5cd6300307c9238db761d9f4c2eb9a2357445
gos <buran1td@mail.ru>
2016-01-15 18:01:25 +0300

    Realy work version

---
+++ Point.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        {
+
+        }
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        public void Move(int offset, Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.LEFT:
+                    x -= offset;
+                    break;
+                case Direction.RIGHT:
+                    x += offset;
+                    break;
+                case Direction.UP:
+                    y -= offset;
+                    break;
+                case Direction.DOWN:
+                    y += offset;
+                    break;
+            }
+        }
+
+        internal void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym; ;
+        }
+
+        internal bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,8 +11,54 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            Console.SetBufferSize(80, 25);
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(26, 9);
+            Console.WriteLine("============================");
+            Console.SetCursorPosition(35, 10);
+            Console.WriteLine("Game over");
+            Console.WriteLine();
+            Console.SetCursorPosition(27, 12);
+            Console.WriteLine("Created by:  Oleg Glytenko");
+            Console.SetCursorPosition(32, 13);
+            Console.WriteLine("My first game!!!");
+            Console.SetCursorPosition(26, 14);
+            Console.WriteLine("============================");
+            Thread.Sleep(1000);
+            Console.ReadKey();
         }
     }
 }
