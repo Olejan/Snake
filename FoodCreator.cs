3c8ac3cc7332b9e8f27c92ea13d12945fddeef83
gos <buran1td@mail.ru>
2016-01-15 17:58:04 +0300

    Snake - work version

---
+++ FoodCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        int mapWidth;
+        int mapHeight;
+        char sym;
+        Random randow = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+        public Point CreateFood()
+        {
+            int x = randow.Next(2, mapWidth - 2);
+            int y = randow.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
---
+++ Walls.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> wallList;
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine hlTop = new HorizontalLine(0, mapWidth - 1, 0, '+');
+            HorizontalLine hlBottom = new HorizontalLine(0, mapWidth - 1, mapHeight - 2, '+');
+            VerticalLine vlLeft = new VerticalLine(0, 0, mapHeight - 2, '+');
+            VerticalLine vlRight = new VerticalLine(mapWidth - 1, 0, mapHeight - 2, '+');
+            wallList.Add(hlTop);
+            wallList.Add(hlBottom);
+            wallList.Add(vlLeft);
+            wallList.Add(vlRight);
+        }
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                    return true;
+            }
+            return false;
+        }
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
